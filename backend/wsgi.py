bfea640a7dfdc76d0120741f07019964c0a20664
Mslprogra <josueferrer2525@gmail.com>
2026-07-18 14:08:05 -0400

    superuser

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -14,3 +14,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    # Cambia 'admin' y 'TuContraseñaSegura123' por los datos que tú quieras
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'expedientesuptai@gmail.com', 'expedientes1')
+        print("¡Superusuario creado con éxito en Render!")
+except Exception as e:
+    print(f"Error al intentar crear el superusuario: {e}")
